d2a247e6cdc8110b26dc4f1e66c18ceeb34c74e1
dikxak <dikshakpoudel76@gmail.com>
2022-06-06 11:44:53 +0545

    add GithubInfos component

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import SearchField from '../components/SearchField';
 
+import GithubInfos from '../components/GithubInfos';
+
 import styles from './SearchPage.module.css';
 
 const SearchPage = () => {
   return (
     <div className={`container ${styles['search-container']}`}>
       <SearchField />
+      <GithubInfos />
     </div>
   );
 };
