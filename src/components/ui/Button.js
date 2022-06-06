f87f4f7ba645930b9ea6079648e387a215ccaff9
dikxak <dikshakpoudel76@gmail.com>
2022-06-06 11:40:46 +0545

    add onClick handler

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -7,6 +7,7 @@ const Button = props => {
     <button
       className={`${styles['btn']} ${props.className}`}
       type={props.type || 'button'}
+      onClick={props.onSubmit}
     >
       {props.children}
     </button>
