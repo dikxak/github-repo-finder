9c0b462de6a795d5a9df9b6ad5c40c3c175c737d
dikxak <dikshakpoudel76@gmail.com>
2022-06-06 11:39:22 +0545

    add icons required and it's css file

---
+++ src/components/icons/Fork.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './icon.css';
+
+const Fork = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="192"
+      height="192"
+      fill="#1864ab"
+      viewBox="0 0 256 256"
+      className="repo-icon"
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path d="M224,68a36,36,0,1,0-44,35.1v.9a16,16,0,0,1-16,16H92a16,16,0,0,1-16-16v-.9a36,36,0,1,0-16,0v.9a32.1,32.1,0,0,0,32,32h28v16.9a36,36,0,1,0,16,0V136h28a32.1,32.1,0,0,0,32-32v-.9A36.1,36.1,0,0,0,224,68ZM148,188a20,20,0,1,1-20-20A20.1,20.1,0,0,1,148,188Z"></path>
+    </svg>
+  );
+};
+
+export default Fork;
